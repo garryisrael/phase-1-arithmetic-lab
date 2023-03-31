5fb72142293de62d7346da472eddf9648edeccd1
Garry <garryisrael02@gmail.com>
2023-03-31 05:17:21 -0400

    Learns Math Class properties

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const num1 = 2; const num2 = 31;
+const num3 = 40; const num4 = 9;
 
-// Write your code here
+let random = Math.ceil(Math.random());
+const multiply = num1*num2;
+const mod = num3 % num4;
+const max = Math.max(13,6,20,15,4,17.11);
+
